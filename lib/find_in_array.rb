75ce755296b0b8879d535506c1f9d9548a193bae
Jassiel Rodriguez <hazer94@gmail.com>
2020-08-09 05:47:56 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   count = 0
   
-  array < count 
-    puts array
+  while count < array.length do 
+    value_to_find = 
+    
     count += 1
-  
+  end
 end
